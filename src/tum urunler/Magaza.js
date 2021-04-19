951e744fa1524a5f69953cdd15d76e386b43f8ef
sebahattin <sebahattin@veriyaz.com.tr>
2021-04-19 12:42:45 +0300

    changes

--- src/tum urunler/Magaza.js
+++ src/tum urunler/Magaza.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-
 import ListItemText from "@material-ui/core/ListItemText";
-import Collapse from "@material-ui/core/Collapse";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
 import useDocumentTitle from "../useDocumentTitle";
+import DataView from "./urunDataViewComponent/DataView";
+import { db } from "./../firebase";
+import firebase from "firebase";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,11 +22,61 @@ const useStyles = makeStyles((theme) => ({
 function Magaza() {
   useDocumentTitle('Mağaza - Herbalife')
   const classes = useStyles();
-  const [openKilo, setOpenKilo] = React.useState(false);
+  const [products, setProducts] = useState([]);
 
-  const [openTakviye, setOpenTakviye] = React.useState(false);
+  const [kiloYonetimi, setKiloYonetimi] = useState([]);
+  const [kisiselBakim, setKisiselBakim] = useState([]);
+  const [takviye, setTakviye] = useState([]);
+  const [tanitim, setTanitim] = useState([]);
 
-  const [openTanitim, setOpenTanitim] = React.useState(false);
+  useEffect(() => {
+    // fires once when the app loads
+    getProducts();
+  }, []);
+
+  const getKiloYonetimi = ()=>{
+    db.collection("products").where("urunKategorisi", "==", 'Kilo Yonetimi')
+    .get()
+    .then((snapshot) => {
+      setKiloYonetimi(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          urunAdi: doc.data().urunAdi,
+          urunKategorisi: doc.data().urunKategorisi,
+          urunResmi: doc.data().urunResmi,
+          urunFiyati: doc.data().urunFiyati,
+          urunIndirimliFiyati: doc.data().urunIndirimliFiyati,
+          urunAciklamasi: doc.data().urunAciklamasi,
+          urunEkbilgisi: doc.data().urunEkbilgisi,
+        }))
+      );
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+  }
+  
+  const getProducts = () => {
+    db.collection("products")
+      .orderBy("timeStamp", "desc")
+      .onSnapshot((snapshot) => {
+        setProducts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            urunAdi: doc.data().urunAdi,
+            urunKategorisi: doc.data().urunKategorisi,
+            urunResmi: doc.data().urunResmi,
+            urunFiyati: doc.data().urunFiyati,
+            urunIndirimliFiyati: doc.data().urunIndirimliFiyati,
+            urunAciklamasi: doc.data().urunAciklamasi,
+            urunEkbilgisi: doc.data().urunEkbilgisi,
+          }))
+        );
+      });
+     
+   
+  };
 
   return (
     <div className="w-100 mx-2">
@@ -48,128 +97,33 @@ function Magaza() {
                       className="w-100 font-weight-bold"
                       style={{ fontSize: "x-large" }}
                     >
-                      {" "}
                       Ürün Kategorileri
                     </p>
                   </ListSubheader>
                 }
                 className={classes.root}
               >
-                <ListItem button onClick={() => setOpenKilo(!openKilo)}>
+                <ListItem button>
                   <ListItemText primary="Kilo Yönetimi" />
-                  {openKilo ? <ExpandLess /> : <ExpandMore />}
                 </ListItem>
 
-                <Collapse in={openKilo} timeout="auto" unmountOnExit>
-                  <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                      <ListItemText primary="Atıştırmalık" />
-                    </ListItem>
-                    <ListItem button className={classes.nested}>
-                      <ListItemText primary="Herbalife Shake" />
-                    </ListItem>
-                    <ListItem button className={classes.nested}>
-                      <ListItemText primary="İçecekler" />
-                    </ListItem>
-                  </List>
-                </Collapse>
 
                 <ListItem button>
                   <ListItemText primary="Kişisel Bakım" />
                 </ListItem>
 
-                <ListItem button onClick={() => setOpenTakviye(!openTakviye)}>
+                <ListItem button>
                   <ListItemText primary="Takviye Edici Gıdalar" />
-                  {openTakviye ? <ExpandLess /> : <ExpandMore />}
                 </ListItem>
 
-                <Collapse in={openTakviye} timeout="auto" unmountOnExit>
-                  <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                      <ListItemText primary="Sporcu Beslenmesi" />
-                    </ListItem>
-                  </List>
-                </Collapse>
-
-                <ListItem button onClick={() => setOpenTanitim(!openTanitim)}>
+                <ListItem button>
                   <ListItemText primary="Tanıtım Ürünleri" />
-                  {openTanitim ? <ExpandLess /> : <ExpandMore />}
                 </ListItem>
 
-                <Collapse in={openTanitim} timeout="auto" unmountOnExit>
-                  <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                      <ListItemText primary="Shaker / Suluklar" />
-                    </ListItem>
-                  </List>
-                </Collapse>
               </List>
             </div>
             <div className="col-lg-9 col-xs-12 ">
-              <div className="row">
-                <div className="col-lg-3 col-sm-4 col-xs-6">
-                  
-                  <div class="card-deck">
-                    <div class="card">
-                      <img
-                        class="card-img-top"
-                        src="https://herbaldamla.com/wp-content/uploads/2021/01/tanisma-paketi-247x247.jpg"
-                        alt="urun adi"
-                      />
-                      <div class="card-body">
-                        <h5 class="card-title text-muted">urun tipi</h5>
-                        <p class="card-text">urun ismi</p>
-                        <p class="card-text font-weight-bold">urun fiyati</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 col-sm-4 col-xs-6">
-                  <div class="card-deck">
-                    <div class="card">
-                      <img
-                        class="card-img-top"
-                        src="https://herbaldamla.com/wp-content/uploads/2021/01/tanisma-paketi-247x247.jpg"
-                        alt="urun adi"
-                      />
-                      <div class="card-body">
-                        <h5 class="card-title text-muted">urun tipi</h5>
-                        <p class="card-text">urun ismi</p>
-                        <p class="card-text font-weight-bold">urun fiyati</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <nav aria-label="...">
-                <ul class="pagination justify-content-center">
-                  <li class="page-item disabled">
-                    <span class="page-link">Previous</span>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      1
-                    </a>
-                  </li>
-                  <li class="page-item active">
-                    <span class="page-link">
-                      2<span class="sr-only">(current)</span>
-                    </span>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      3
-                    </a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      Next
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+                <DataView/>
             </div>
           </div>
         </div>
